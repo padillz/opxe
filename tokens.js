91b391a62543cc1f8236322a4af4271431349680
dilzx <padillz575@gmail.com>
2025-12-20 11:51:24 +0700

    Update token list

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "122344555:hgxghjkkkknnjj",
-    "8001616412:AAE40A1EYv7OXIuVuP7MXgqrvvT0V0-VkTg"
+    "8001616412:AAE40A1EYv7OXIuVuP7MXgqrvvT0V0-VkTg",
+    "8200995263:AAE6o3p5Dkx_ZaUE1cZlVti9f1GFtCM-ZSQ"
   ]
 }
