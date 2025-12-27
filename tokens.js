6a91a30ff8000ba508709b7e7b0521172b1f1d9b
dilzx <padillz575@gmail.com>
2025-12-27 21:59:51 +0700

    Update token list

--- tokens.js
+++ tokens.js
@@ -5,6 +5,7 @@
     "8200995263:AAE6o3p5Dkx_ZaUE1cZlVti9f1GFtCM-ZSQ",
     "7139797560:AAGj7_-4SOhCx8llauFh6spUFkw0YlP3a-w",
     "8275555794:AAHz-yiHQurrSdzTzRXlECoJkxEC7RdHEOk",
-    "8290566531:AAFgxcvzyaEp7I2iKUwzAr3dTSg5Z6kINnk"
+    "8290566531:AAFgxcvzyaEp7I2iKUwzAr3dTSg5Z6kINnk",
+    "8226169861:AAGiAyYHbBg_YPbuQZxFggtMGfkqxjM95ls"
   ]
 }
